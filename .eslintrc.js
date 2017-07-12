c3bb42df964bd338ac803489a80914ed52fead4d
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2017-07-12 23:43:07 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+
+  extends: "airbnb-base",
+
+  parser: "babel-eslint",
+
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: "module",
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      impliedStrict: true
+    }
+  },
+
+  plugins: [
+    "promise",
+    "import"
+  ],
+
+  rules: {
+    "promise/avoid-new": "warn",
+    "promise/catch-or-return": "error",
+    "promise/param-names": "error",
+    "promise/no-promise-in-callback": "warn",
+    "promise/no-callback-in-promise": "warn"
+  }
+}
